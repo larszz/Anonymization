ec43013e070756adbb1dbfcd63028bdb80a399b8
Lars Zinz <la.zi@t-online.de>
2020-01-15 11:10:22 +0100

    Added some logging functions
    Signed-off-by: Lars Zinz <la.zi@t-online.de>

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,5 @@
+import logging as log
+
 class Messages:
 	NONETYPEERROR = '"{0}" is NoneType!'
 	COLUMNNAMEERROR = 'Column names could not be read from file {0}'
@@ -24,3 +26,21 @@ class ColumnNameError(Exception):
 
 	def __init__(self, filename):
 		self.message = Messages.COLUMNNAMEERROR.format(str(filename))
+
+
+
+class Logger:
+	def log_none_type(self, fieldname: str=None):
+		if fieldname is None:
+			return
+
+		log.warning(Messages.NONETYPEERROR.format(fieldname))
+
+
+	def log_instance_error(self, obj: str=None, type: str=None):
+		if (obj is None) or (type is None):
+			self.log_none_type("Object or type")
+			return
+
+		log.error(Messages.INSTANCEERROR.format(obj, type))
+
