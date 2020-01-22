66c8798b49d8c327af56060820281a436e63dbe5
Lars Zinz <la.zi@t-online.de>
2020-01-22 12:19:36 +0100

    adding xml name constants
    Signed-off-by: Lars Zinz <la.zi@t-online.de>

--- values.py
+++ values.py
@@ -12,3 +12,20 @@ class delimiters:
 		PRIMARY = ';'
 		SECONDARIES = [',', ';']
 		QUOTECHAR = '"'
+
+
+class xml_names:
+	INPUT_DIR = 'inputdirectory'
+	OUTPUT_DIR = 'outputdirectory'
+	TABLE = 'table'
+	TABLENAME = 'tablename'
+	COLUMNS_ANONYM = 'columns_anonym'
+	COLUMN = 'column'
+	NAME = 'name'
+	PATTERN = 'pattern'
+	FRONT = 'front'
+	END = 'end'
+	COLUMNS_PSEUDONYM = 'columns_pseudonym'
+	LINK = 'link'
+	NEW_FIELD = 'newfield'
+	READABLE = 'readable'
